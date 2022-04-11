149768f91c0a4d99ec0dad31f88fd06e678b4106
Tomwf <chowkirthmas@gmail.com>
2022-04-12 00:22:23 +0200

    test: add tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -71,10 +71,20 @@ suite('UnitTests', () => {
   })
 
   suite('solve()', () => {
+    test('valid puzzle pass the solver', () => {
+      assert.isString(solver.solve(validPuzzleString))
+    })
+
     test('solve the puzzle', () => {
       const solution = '769235418851496372432178956174569283395842761628713549283657194516924837947381625'
 
       assert.strictEqual(solver.solve(validPuzzleString), solution)
+    })
+
+    test('invalid puzzle', () => {
+      const invalidPuzzleString = '..8..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
+
+      assert.deepEqual(solver.solve(invalidPuzzleString), 'Puzzle cannot be solved')
     })
   })
 });
